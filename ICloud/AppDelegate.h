db447b9fc4fa4c9502d41c56a80de1833ee7c673
Bala29 <balamurugan2999@gmail.com>
2017-07-26 16:24:40 +0530

    Initial Commit

---
+++ ICloud/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ICloud
+//
+//  Created by Bala on 26/07/17.
+//  Copyright © 2017 Erabala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ICloud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ICloud
+//
+//  Created by Bala on 26/07/17.
+//  Copyright © 2017 Erabala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
